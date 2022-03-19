00436519201c9689805fdc41204310a3dded2ebb
MEGABYETE <101935068+MEGABYETE@users.noreply.github.com>
2022-03-19 15:15:14 +0100

    Add files via upload

---
+++ Basic_calculator.py
@@ -0,0 +1,10 @@
+first_number=int(input("insert your first_number"))
+second_number=int(input("insert your second_number"))
+addition=first_number+second_number
+division=first_number/second_number
+subtraction=first_number-second_number
+multiplication=first_number*second_number
+print(addition)
+print(division)
+print(subtraction)
+print(multiplication)
